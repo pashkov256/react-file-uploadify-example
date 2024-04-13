09841a06a03b364ad3799e15e76ccdc74a9cd48b
temaweb10 <pashkovjob@gmail.com>
2024-04-13 20:23:23 +0300

    Updates

---
+++ precache-manifest.ca05f24006831a201756963228d6cfef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c68595241e5a88f7991fab14507634",
+    "url": "/react-file-uploadify-example/index.html"
+  },
+  {
+    "revision": "990285164dc525a9fd5a",
+    "url": "/react-file-uploadify-example/static/css/2.c3a87cb8.chunk.css"
+  },
+  {
+    "revision": "17a3fc13396b5911d43c",
+    "url": "/react-file-uploadify-example/static/css/main.980713ee.chunk.css"
+  },
+  {
+    "revision": "990285164dc525a9fd5a",
+    "url": "/react-file-uploadify-example/static/js/2.b6ff24dd.chunk.js"
+  },
+  {
+    "revision": "17a3fc13396b5911d43c",
+    "url": "/react-file-uploadify-example/static/js/main.a5b6f7fe.chunk.js"
+  },
+  {
+    "revision": "425ae0ea84e12bfb3319",
+    "url": "/react-file-uploadify-example/static/js/runtime~main.0f180a89.js"
+  }
+]);
